e04acad9f6a05dc676db203b8359892f7ba30094
KulaiFood Admin <admin@kulaifood.local>
2026-01-24 15:20:07 +0800

    Update Support QR code image URL

--- src/components/SupportModal.jsx
+++ src/components/SupportModal.jsx
@@ -88,7 +88,7 @@ const SupportModal = ({ isOpen, onClose, isAdmin, supportQR, onUpdateQR }) => {
                     {/* QR Code Image */}
                     {!imgError ? (
                         <img 
-                            src={supportQR || "https://i.ibb.co/LXb1FHhH/JNQ-MEDIA-TNG.jpg"} 
+                            src={supportQR || "https://i.ibb.co/CpzRJY1M/TNG-QR-JNQ-MEDIA.jpg"} 
                             alt="Touch 'n Go QR Code" 
                             className="w-full h-auto object-contain max-h-[400px]"
                             onError={(e) => {
